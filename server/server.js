eca67a7ee26eff5e9c6fe5ea82dce4a0d0bbac87
Travis Frank <travis@travismfrank.com>
2020-08-15 13:25:21 -0400

    Setup router and controller, implemented POST route to serve election data

--- server/server.js
+++ server/server.js
@@ -1,7 +1,37 @@
 const path = require('path');
 const express = require('express');
+const buddyRouter = require('./routes/buddyRouter');
 
 const app = express();
+
+// Parses incoming JSON bodies
+app.use(express.json());
+
+// Static files
+app.use('/assets', express.static(path.resolve(__dirname, '../client/assets')));
+
+// Main App
+app.get('/', (req, res) => res.status(200).sendFile(path.resolve(__dirname, '../index.html')));
+
+// Handles requests for election info
+app.use('/api', buddyRouter);
+
+// 404 Handler
+app.use((req,res) => {
+  res.sendStatus(404);
+});
+
+// Global Error Handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 400,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
 app.listen(3000, () => {
   console.log('your voting friend: listening on 3000');
