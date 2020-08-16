998a30d5ef59fb15231230209555e7c3bea32de7
Travis Frank <travis@travismfrank.com>
2020-08-16 10:34:23 -0400

    Added routes for getting senator + representative data

--- server/controllers/buddyController.js
+++ server/controllers/buddyController.js
@@ -24,6 +24,8 @@ buddyController.getUpcomingElectionData = (req, res, next) => {
         res.locals.electionName = data.election.name;
         res.locals.electionDay = data.election.electionDay;
         res.locals.normalizedAddress = data.normalizedInput;
+        res.locals.pollingLocations = data.pollingLocations;
+        res.locals.earlyVoteSites = data.earlyVoteSites;
         const officeContests = [];
         const referendaContests = [];
         data.contests.forEach(cv => {
@@ -58,6 +60,109 @@ buddyController.getUpcomingElectionData = (req, res, next) => {
       message: { err: `An error occurred` },
     })
   }
-}
+};
+
+buddyController.getCurrentSenators = (req, res, next) => {
+  let url = 'https://api.propublica.org/congress/v1/members/senate/'
+  url += req.body.state;
+  url += '/current.json';
+
+  console.log('Making a request to: ' + url);
+
+  try {
+    fetch(url, {
+      headers: {
+        "X-API-Key": process.env.PROPUBLICA_API_KEY
+      }
+    })
+      .then(data => data.json())
+      .then(data => {
+        res.locals.senatorUris = [data.results[0].api_uri, data.results[1].api_uri];
+        return next();
+      })
+  } catch (err) {
+    return next({
+      log: `An error occured in buddyController.getCurrentSenators: ${err}`,
+      message: { err: `An error occurred` },
+    })
+  }
+};
+
+buddyController.getFurtherSenatorInfoOne = (req, res, next) => {
+    try {
+      console.log('Making a request to: ' + res.locals.senatorUris[0]);
+      fetch(res.locals.senatorUris[0], {
+        headers: {
+          "X-API-Key": process.env.PROPUBLICA_API_KEY
+        }
+      })
+        .then(data => data.json())
+        .then(data => {
+          res.locals.senatorOne = data.results[0];
+          res.locals.senatorOne.currentRole = res.locals.senatorOne.roles[0];
+          delete res.locals.senatorOne.roles;
+          return next();
+        })
+    } catch (err) {
+      return next({
+        log: `An error occured in buddyController.getFurtherSenatorInfoOne: ${err}`,
+        message: { err: `An error occurred` },
+      })
+    }
+  };
+
+
+buddyController.getFurtherSenatorInfoTwo = (req, res, next) => {
+    try {
+      console.log('Making a request to: ' + res.locals.senatorUris[1]);
+      fetch(res.locals.senatorUris[1], {
+        headers: {
+          "X-API-Key": process.env.PROPUBLICA_API_KEY
+        }
+      })
+        .then(data => data.json())
+        .then(data => {
+          res.locals.senatorTwo = data.results[0];
+          res.locals.senatorTwo.currentRole = res.locals.senatorTwo.roles[0];
+          delete res.locals.senatorTwo.roles;
+          return next();
+        })
+    } catch (err) {
+      return next({
+        log: `An error occured in buddyController.getFurtherSenatorInfoTwo: ${err}`,
+        message: { err: `An error occurred` },
+      })
+    }
+};
+
+buddyController.getCurrentRepresentatives = (req, res, next) => {
+  let url = 'https://www.googleapis.com/civicinfo/v2/representatives?key='
+  url += process.env.CIVIC_API_KEY;
+  url += '&address=';
+  const address = req.body.addressLine.split(' ').join('%20');
+  url += address;
+  url += '%20';
+  url += req.body.city;
+  url += '%20';
+  url += req.body.state;
+  url += '%20';
+  url += req.body.zip;
+
+  console.log('Making a request to: ' + url);
+
+  try {
+    fetch(url)
+      .then(data => data.json())
+      .then(data => {
+        res.locals.officials = data.officials.slice(2);
+        return next();
+      })
+  } catch (err) {
+    return next({
+      log: `An error occured in buddyController.getDistrict: ${err}`,
+      message: { err: `An error occurred` },
+    })
+  }
+};
 
 module.exports = buddyController;
