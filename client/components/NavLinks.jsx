2189e7a9c8311ac165236a20a2c34bb16eeac0c6
Rebecca Miller <beemills@gmail.com>
2020-08-17 18:09:03 -0400

    last one before pulling upstream master

--- client/components/NavLinks.jsx
+++ client/components/NavLinks.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 import FacebookLogin from 'react-facebook-login';
+
 const NavLinks = ({ isLoggedIn }) => {
 
   return (
     <div className="nav-links">
-      {/* <a href="/auth/facebook">Login with Facebook</a> */}
+      <a href="/auth/facebook">Login with Facebook</a>
       <div className="fb-share-button" data-href="https://www.cnn.com" data-layout="button" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A8080%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a></div>
     </div>
       // <FacebookLogin
