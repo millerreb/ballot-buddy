860ae680a2716f92fe6eaf41d195c906b9ea172a
Rebecca Miller <beemills@gmail.com>
2020-08-17 11:18:44 -0400

    react facebook login with component button added

--- client/components/NavLinks.jsx
+++ client/components/NavLinks.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
-
+import FacebookLogin from 'react-facebook-login';
 const NavLinks = ({ isLoggedIn }) => {
   
   return (
-    <div className="nav-links">
-
-      { isLoggedIn && <Button variant="contained" size="small" color="secondary">Profile</Button> }
-      { isLoggedIn ? <Button variant="contained" size="small" color="secondary">Logout</Button> : <Button variant="contained" size="small" color="primary">Sign-in</Button> }
-    </div>
+    // <div className="nav-links">
+    //   { isLoggedIn && <Button variant="contained" size="small" color="secondary">Profile</Button> }
+    //   { isLoggedIn ? <Button variant="contained" size="small" color="secondary">Logout</Button> : <Button variant="contained" size="small" color="primary">Sign-in</Button> }
+    // </div>
+      <FacebookLogin
+      appId="318062672725904"
+      autoLoad={true}
+      fields="name,email,picture"
+      onClick={componentClicked}
+      callback={responseFacebook} />
   );
 };
 
